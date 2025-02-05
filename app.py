d81e294b44c4253705e0e8d205c5b2caaaa1283e
alison <alison.filgueiras@ueg.br>
2025-02-05 15:54:42 -0300

    0502

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from blueprints.classapi import classapi_app
 from blueprints.objectapi import objectapi_app
 from blueprints.acesso import acessoapp
 from blueprints.repositorios import repo_app
+from blueprints.upload import uploadapp
 import ssl
 import os
 from dotenv import load_dotenv
@@ -14,9 +15,23 @@ from urllib3.exceptions import InsecureRequestWarning
 # Carregar variáveis de ambiente do arquivo .env
 load_dotenv()
 
+
+
+
+
 app = Flask(__name__)
 CORS(app)
 CORS(app, resources={r"/*": {"origins": ["https://localhost:9000"]}})
+
+
+app.config['UPLOAD_FOLDER'] = '/arquivos/upload'
+
+# Outras configurações (opcional)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # Limite de 16MB por arquivo
+app.config['ALLOWED_EXTENSIONS'] = {'jpg', 'jpeg', 'png', 'gif', 'mp4'}  # Extensões permitidas
+
+
+
 
 # Registrar os aplicativos Flask dos serviços
 app.register_blueprint(sparqapi_app, url_prefix='/sparqapi')
@@ -24,6 +39,7 @@ app.register_blueprint(classapi_app, url_prefix='/classapi')
 app.register_blueprint(objectapi_app, url_prefix='/objectapi')
 app.register_blueprint(acessoapp, url_prefix='/acesso')
 app.register_blueprint(repo_app, url_prefix='/repositorios')
+app.register_blueprint(uploadapp, url_prefix='/uploadapp')
 
 if __name__ == '__main__':
     # Desativar os avisos de requisições inseguras (opcional)
--- blueprints/upload.py
+++ blueprints/upload.py
@@ -1,21 +1,46 @@
 from flask import Blueprint, request, jsonify, current_app
-import os
+import os, uuid
 from werkzeug.utils import secure_filename
+
 uploadapp = Blueprint('uploadapp', __name__)
 
-
 @uploadapp.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
+def upload_files():
+    # Obtém o ID do objeto a partir do formulário
+    objeto_id = request.form.get('objetoId')
+    if not objeto_id:
+        return jsonify({'error': 'ID do objeto não fornecido'}), 400
 
-    file = request.files['file']
 
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
+    upload_folder = current_app.config.get('UPLOAD_FOLDER')
+    objeto_folder = os.path.join(upload_folder, str(objeto_id))
 
-    if file:
-        filename = secure_filename(file.filename)
-        file_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-        return jsonify({'message': 'File uploaded successfully', 'file_path': file_path}), 200
+    if not os.path.exists(objeto_folder):
+        os.makedirs(objeto_folder)
+
+    if 'midias' not in request.files:
+        return jsonify({'error': 'Nenhuma mídia enviada'}), 400
+
+    arquivos = request.files.getlist('midias')
+  
+    if not arquivos or all(file.filename == '' for file in arquivos):
+        return jsonify({'error': 'Nenhum arquivo selecionado'}), 400
+
+    arquivos_salvos = []
+
+    # Processa cada arquivo
+    for file in arquivos:
+        if file.filename:
+            # Gera um nome seguro para o arquivo
+            extensao = os.path.splitext(file.filename)
+            filename = secure_filename(f"{uuid.uuid4().hex}{extensao}")
+            file_path = os.path.join(objeto_folder, filename)
+
+            # Salva o arquivo no servidor
+            file.save(file_path)
+            arquivos_salvos.append(filename)  # Armazena apenas o nome do arquivo
+
+    return jsonify({
+        'message': 'Arquivos enviados com sucesso!',
+        'arquivos': arquivos_salvos  # Retorna a lista de nomes dos arquivos salvos
+    }), 200
